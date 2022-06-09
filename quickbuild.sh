a2e18c9d3cf8dfa6a65088d36201aa310ac14807
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-06-09 09:12:00 +0200

    Added arm compilation

--- quickbuild.sh
+++ quickbuild.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "Compiling C"
+echo "Compiling C-x86"
 gcc c/main.c -o wrongsecrets-c
+echo "Compiling C-ARM, based on https://github.com/dockcross/dockcross"
+echo "prerequired: git clone https://github.com/dockcross/dockcross.git"
+echo "prerequired: cd dockcross"
+echo "prerequired: docker run --rm dockcross/linux-arm64 > ./dockcross-linux-arm64"
+echo "prerequired: chmod +x ./dockcross-linux-arm64 && mv ./dockcross-linux-arm64 .. && cd .."
+./dockcross-linux-arm64 bash -c '$CC c/main.c -o wrongsecrets-c-arm'
