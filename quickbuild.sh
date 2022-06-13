fb7b4944b3d0cd9e67c7997bfc28fdfb965777ae
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-06-13 09:55:41 +0200

    addedlinux compile:

--- quickbuild.sh
+++ quickbuild.sh
@@ -8,3 +8,8 @@ echo "prerequired: cd dockcross"
 echo "prerequired: docker run --rm dockcross/linux-arm64 > ./dockcross-linux-arm64"
 echo "prerequired: chmod +x ./dockcross-linux-arm64 && mv ./dockcross-linux-arm64 .. && cd .."
 ./dockcross-linux-arm64 bash -c '$CC c/main.c -o wrongsecrets-c-arm'
+echo "Compiling C for linux"
+echo "prerequired: cd dockcross"
+echo "prerequired: docker run --rm dockcross/linux-x64 > ./dockcross-linux-x64"
+echo "prerequired: chmod +x ./dockcross-linux-x64 && mv ./dockcross-linux-x64 .. && cd .."
+./dockcross-linux-x64 bash -c '$CC c/main.c -o wrongsecrets-c-linux'
