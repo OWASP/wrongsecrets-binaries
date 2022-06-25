1608686c1a05fc84db0c6268c329728a88dd9a06
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-06-25 22:11:10 +0200

    Cleanup golang license stuff

--- golang/cmd/guess.go
+++ golang/cmd/guess.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -32,14 +28,4 @@ var guessCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(guessCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// guessCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// guessCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- golang/cmd/spoil.go
+++ golang/cmd/spoil.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -22,14 +18,4 @@ var spoilCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(spoilCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// spoilCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// spoilCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
