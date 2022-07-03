688423f123ddca50db116ccb428fb88688840b83
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-07-03 06:27:28 +0200

    Renamed all builds and added arm darwin golang for https://github.com/commjoen/wrongsecrets/issues/340

--- quickbuild.sh
+++ quickbuild.sh
@@ -2,13 +2,13 @@
 
 echo "Compiling for current machine"
 gcc c/main.c -o wrongsecrets-c
-echo "Compiling C for ARM, based on https://github.com/dockcross/dockcross"
+echo "Compiling C for ARM-linux, based on https://github.com/dockcross/dockcross"
 echo "prerequired: git clone https://github.com/dockcross/dockcross.git"
 echo "prerequired: cd dockcross"
 echo "prerequired: docker run --rm dockcross/linux-arm64-lts > ./dockcross-linux-arm64-lts"
 echo "prerequired: chmod +x ./dockcross-linux-arm64-lts && mv ./dockcross-linux-arm64-lts .. && cd .."
-./dockcross-linux-arm64-lts bash -c '$CC c/main.c -o wrongsecrets-c-arm'
-echo "Compiling C for linux"
+./dockcross-linux-arm64-lts bash -c '$CC c/main.c -o wrongsecrets-c-linux-arm'
+echo "Compiling C for x86-linux"
 echo "prerequired: cd dockcross"
 echo "prerequired: docker run --rm dockcross/linux-x64 > ./dockcross-linux-x64"
 echo "prerequired: chmod +x ./dockcross-linux-x64 && mv ./dockcross-linux-x64 .. && cd .."
@@ -17,7 +17,7 @@ echo "prerequired: chmod +x ./dockcross-linux-x64 && mv ./dockcross-linux-x64 ..
 echo "Compiling C++ for current machine"
 gcc cplus/main.cpp -lstdc++ -o wrongsecrets-cplus
 echo "Compiling C++ for ARM, based on https://github.com/dockcross/dockcross"
-./dockcross-linux-arm64-lts bash -c '$CC cplus/main.cpp -lstdc++ -o wrongsecrets-cplus-arm'
+./dockcross-linux-arm64-lts bash -c '$CC cplus/main.cpp -lstdc++ -o wrongsecrets-cplus-linux-arm'
 echo "Compiling C++ for linux"
 ./dockcross-linux-x64 bash -c '$CC cplus/main.cpp -lstdc++ -o wrongsecrets-cplus-linux'
 
@@ -26,6 +26,8 @@ cd golang
 echo "compiling golang for amd64 linux"
 env GOOS=linux GOARCH=amd64 go build -o ../wrongsecrets-golang-linux
 echo "compiling golang for arm linux"
-env GOOS=linux GOARCH=arm64 go build -o ../wrongsecrets-golang-arm
+env GOOS=linux GOARCH=arm64 go build -o ../wrongsecrets-golang-linux-arm
 echo "compiling golang for mac os x (intel)"
 env GOOS=darwin GOARCH=amd64 go build -o ../wrongsecrets-golang
+echo "compiling golang for mac os x (ARM)"
+env GOOS=darwin GOARCH=amd64 go build -o ../wrongsecrets-golang-arm
