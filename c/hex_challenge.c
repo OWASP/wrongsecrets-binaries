cbd33db4cf8ef059172fe5b2d616d6b9e715de9d
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-06-22 15:10:36 -0300

    Refactor the script
    I added a xor function in order to add simple xor encryption
    Added a spoil and execute function

--- c/hex_challenge.c
+++ c/hex_challenge.c
@@ -1,37 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SIZE 21
 
 char *decode(const char ** )__attribute__((visibility("hidden")));
-int compare(const char *, const char * )__attribute__((visibility("hidden")));
-void usage(char *);
+void usage(char *)__attribute__((visibility("hidden")));
+void execute(char *)__attribute__((visibility("hidden")));
+void spoil(void)__attribute__((visibility("hidden")));
+char *xor(char *, int )__attribute__((visibility("hidden")));
+
+const char *secret[] = {"69","67","6f","74","68","61","63","6b","65","64","62","79","73","65","63","72","65","74","70","77","64"};
 
 int main(int argc, char **argv)
 {	
-	const char *secret[] = {"69","67","6f","74","68","61","63","6b","65","64","62","79","73","65","63","72","65","74","70","77","64"};
-	char *encoded = decode(secret);
-	
 	if(argc < 2)
 	{
 		usage(argv[0]);
 	}
 	
-	puts(compare(encoded, argv[1]) ? "root:~# You're root :-D" : "Try again");
+	execute(argv[1]);
 	
 	return EXIT_SUCCESS;
+}
+char *xor(char *string, int key)
+{
+	int size = strlen(string);
+	char *ret = (char *)calloc(strlen(string),sizeof(int));
+	
+	for(int a = 0; a < size;a++)
+		ret[a] = string[a] ^ key;
+	
+	return ret;
+}
+void spoil(void)
+{
+	printf("This is a hardcoded secret game in C\n");
+	exit(EXIT_SUCCESS);
 }
 void usage(char *string)
 {
 	printf("Usage: %s [PASSWORD]\n",string);
+	printf("Usage: %s spoil \n",string);
 	exit(EXIT_FAILURE);
 }
-int compare(const char *encoded,const char *passwd)
+void execute(char *command)
 {
-	for(int a = 0; passwd[a];a++)
-		if(encoded[a] != passwd[a])
-			return 0;
-	return 1;
+	if(strcmp(command,"spoil") == 0)
+	{
+		spoil();
+		return;
+	}
+	if(strcmp(command,xor(decode(secret),2)) == 0 )
+	{
+		printf("root:~# You're root :-D\n");
+	}
+	else if(strcmp(command,decode(secret)) == 0)
+	{
+		puts("You're almost there champion! Here's two hints: ");
+		puts("#1: How many different elements there exists in a binary set ?");
+		puts("#2: You can have a computer or bike, but cannot have both.");
+	}
+	else
+	{
+		puts("Try again");
+	}
 }
 char *decode(const char **string)
 {	
