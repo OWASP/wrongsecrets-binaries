8d9b3444e6b69622ec7e22dee8036e61dbdbd89b
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2024-02-24 08:16:43 +0100

    github action for mac and linux for  https://github.com/OWASP/wrongsecrets/issues/615

--- swift/Sources/main.swift
+++ swift/Sources/main.swift
@@ -1,4 +1,25 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+func getSecret() -> String {
+    let CharArr: [Character] = ["T", "h", "i", "s", " ", "a", " ", "s", "e", "c", "r", "e", "t"] 
 
-print("Hello, world!")
+    let newStr = String(CharArr) 
+    return newStr
+}
+if(CommandLine.arguments.count == 2){
+    if (CommandLine.arguments[1] == "spoil"){
+        print(getSecret())
+    } else {
+        if (CommandLine.arguments[1] == getSecret()){
+            print("This is correct! Congrats!");
+        }else{
+            print("This is incorrect. Try again");
+        }
+    }
+} else if (CommandLine.arguments.count==1) {
+    print("Welcome to the wrongsecrets Swift binary which hides a secret.");
+    print("Use args spoil or a string to guess the password.");
+} else {
+    print("Too many arguments supplied.");
+}
+
