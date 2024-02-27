5d950a97c5122270b90c22a064d32fa62fe2e4b7
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2024-02-27 05:51:46 +0100

    Setting up initial appcode for dotnet as i could not sleep https://github.com/OWASP/wrongsecrets/issues/1243

--- dotnet/dotnetproject/Program.cs
+++ dotnet/dotnetproject/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+if(args.Length==0){
+    Console.WriteLine("Welcome to the wrongsecrets dotnet binary which hides a secret.");
+    Console.WriteLine("Use args spoil or a string to guess the password.");
+} else {
+    if(args[0] == "spoil"){
+        Console.WriteLine("spoil called");
+    } else {
+        Console.WriteLine(args[0]); 
+    }
+}
+
+
