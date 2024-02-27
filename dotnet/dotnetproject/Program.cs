4987365299304911f281c18c534ac29e18800bc7
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2024-02-28 00:34:44 +0100

    setup initial challenge https://github.com/OWASP/wrongsecrets/issues/1243

--- dotnet/dotnetproject/Program.cs
+++ dotnet/dotnetproject/Program.cs
@@ -1,13 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-if(args.Length==0){
-    Console.WriteLine("Welcome to the wrongsecrets dotnet binary which hides a secret.");
-    Console.WriteLine("Use args spoil or a string to guess the password.");
-} else {
-    if(args[0] == "spoil"){
-        Console.WriteLine("spoil called");
-    } else {
-        Console.WriteLine(args[0]); 
+﻿
+String getSecret()
+{
+    return "This is a dotnet secret, huray.";
+}
+
+String evalAnswer(String answer)
+{
+    if (String.Compare(answer, getSecret()) == 0)
+    {
+        return "This is correct! Congrats!";
+    }
+    else
+    {
+        return "This is incorrect. Try again";
     }
 }
 
+if (args.Length == 0)
+{
+    Console.WriteLine("Welcome to the wrongsecrets dotnet binary which hides a secret.");
+    Console.WriteLine("Use args spoil or a string to guess the password.");
+}
+else
+{
+    if (args[0] == "spoil")
+    {
+        Console.WriteLine("spoil called");
+        Console.WriteLine(getSecret());
+    }
+    else
+    {
+        Console.WriteLine(evalAnswer(args[0]));
+    }
+}
 
