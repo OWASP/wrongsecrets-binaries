a86621fee0723ad2f3d0a3064c7ec40143c240fa
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2025-03-01 08:07:26 +0100

    first swift6 test

--- swift/Package.swift
+++ swift/Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version: 6.0.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "wrongsecrets-swift",
+    name: "swift",
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "wrongsecrets-swift"),
+            name: "swift"),
     ]
 )
