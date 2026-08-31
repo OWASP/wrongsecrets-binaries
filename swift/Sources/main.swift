17f4b03d4d98b72306b3e6c737083f18a7195410
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2026-08-31 06:27:44 +0200

    Fix swift issues

--- swift/Sources/main.swift
+++ swift/Sources/main.swift
@@ -2,25 +2,26 @@
 // https://docs.swift.org/swift-book
 
 func getSecret() -> String {
-    let CharArr: [Character] = ["T", "h", "i", "s", " ", "a", " ", "s", "e", "c", "r", "e", "t"] 
-
-    let newStr = String(CharArr) 
-    return newStr
+    let characters: [Character] = ["T", "h", "i", "s", " ", "a", " ", "s", "e", "c", "r", "e", "t"]
+    return String(characters)
 }
-if(CommandLine.arguments.count == 2){
-    if (CommandLine.arguments[1] == "spoil"){
+
+let arguments = CommandLine.arguments
+
+switch arguments.count {
+case 1:
+    print("Welcome to the wrongsecrets Swift binary which hides a secret.")
+    print("Use args spoil or a string to guess the password.")
+case 2:
+    let suppliedValue = arguments[1]
+    if suppliedValue == "spoil" {
         print(getSecret())
+    } else if suppliedValue == getSecret() {
+        print("This is correct! Congrats!")
     } else {
-        if (CommandLine.arguments[1] == getSecret()){
-            print("This is correct! Congrats!");
-        }else{
-            print("This is incorrect. Try again");
-        }
+        print("This is incorrect. Try again")
     }
-} else if (CommandLine.arguments.count==1) {
-    print("Welcome to the wrongsecrets Swift binary which hides a secret.");
-    print("Use args spoil or a string to guess the password.");
-} else {
-    print("Too many arguments supplied.");
+default:
+    print("Too many arguments supplied.")
 }
 
